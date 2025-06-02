24517146aaf476c15b3c965f498761781b9d1a8e
dlworua <leegaegyum@gmail.com>
2025-06-02 16:04:48 +0900

    feat: 컨테이너 위젯 제거

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0XFFFFFDF4),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
-        children: [Container(child: Image.asset('assets/appbar_logo.png'))],
+        children: [Image.asset('assets/appbar_logo.png')],
       ),
     );
   }
