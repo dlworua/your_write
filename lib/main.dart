961b94df5fabde4ebe12c78616e88a920b2efcba
dlworua <leegaegyum@gmail.com>
2025-06-05 09:16:22 +0900

    docs: 이름변경된 파일 임포트문 제거

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:your_write/ui/pages/home/home_page.dart';
-import 'package:your_write/ui/pages/write/write_page.dart';
 
 void main() {
   runApp(ProviderScope(child: MyApp()));
