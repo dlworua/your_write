3d344015cf1587cf22a7788bc15731ef5a43e334
dlworua <leegaegyum@gmail.com>
2025-06-05 10:30:18 +0900

    feat: 게시글 위젯 하단 구성

---
+++ lib/ui/pages/random_post/widgets/random_post_bottom.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class RandomPostBottom extends StatelessWidget {
+  const RandomPostBottom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80,
+      width: 400,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(50),
+          bottomRight: Radius.circular(50),
+        ),
+        border: Border.all(color: Colors.grey, width: 0.5),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(padding: EdgeInsets.only()),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.favorite_border_rounded),
+          ),
+          Text('37'),
+          IconButton(onPressed: () {}, icon: Icon(Icons.comment)),
+          Text('326'),
+          IconButton(onPressed: () {}, icon: Icon(Icons.share)),
+
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.bookmark_outline_rounded),
+          ),
+        ],
+      ),
+    );
+  }
+}
