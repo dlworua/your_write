a8d88937d879cf0b2904391d433501ca04030992
dlworua <leegaegyum@gmail.com>
2025-06-05 10:29:28 +0900

    feat: 상단 배너 로고 삽입

---
+++ lib/ui/pages/random_post/random_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class RandomPage extends StatelessWidget {
+  const RandomPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0XFFFFFDF4),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset('assets/appbar_logo.png'),
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              children: [],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
