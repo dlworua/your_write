f5b7c9e9bb9ed1ffffe6e668f0551673e72e304c
dlworua <leegaegyum@gmail.com>
2025-06-09 15:31:58 +0900

    feat: 메인페이지 파일 생성해서 바텀 네비게이션바 ui구성 및 페이지 이동 구현

---
+++ lib/ui/pages/main_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:your_write/ui/pages/ai_post/ai_page.dart';
+import 'package:your_write/ui/pages/home/home_page.dart';
+import 'package:your_write/ui/pages/random_post/random_page.dart';
+import 'package:your_write/ui/widgets/bottom_navigation_bar.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _currentIndex = 1;
+  final PageController _pageController = PageController(initialPage: 1);
+
+  final List<Widget> _pages = const [AiPage(), HomePage(), RandomPage()];
+
+  void _onTabTapped(int index) {
+    _pageController.animateToPage(
+      index,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+    );
+    setState(() => _currentIndex = index);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: _pages,
+      ),
+      bottomNavigationBar: BottomNavBar(
+        currentIndex: _currentIndex,
+        onTap: _onTabTapped,
+      ),
+    );
+  }
+}
