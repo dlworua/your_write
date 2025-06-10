9c814201814923653b2d6075b59ad1d7dc036d61
dlworua <leegaegyum@gmail.com>
2025-06-10 14:41:23 +0900

    feat: 각 게시판 각 글쓰기 페이지와 연결

--- lib/ui/pages/ai_post/ai_page.dart
+++ lib/ui/pages/ai_post/ai_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:your_write/ui/pages/ai_post/widgets/ai_post_list.dart';
+import 'package:your_write/ui/pages/write/widgets/ai_write_page.dart';
 
 class AiPage extends StatelessWidget {
   const AiPage({super.key});
@@ -11,7 +12,41 @@ class AiPage extends StatelessWidget {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Image.asset('assets/appbar_logo.png'),
+          Stack(
+            children: [
+              Image.asset('assets/appbar_logo.png'),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return AiWritePage();
+                      },
+                    ),
+                  );
+                },
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 85, left: 325),
+                      child: Icon(Icons.edit, size: 20),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 315),
+                      child: Text(
+                        '글쓰기',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
           Expanded(
             child: ListView(
               padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
--- lib/ui/pages/random_post/random_page.dart
+++ lib/ui/pages/random_post/random_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:your_write/ui/pages/random_post/widgets/random_post_list.dart';
+import 'package:your_write/ui/pages/write/random_write_page.dart';
 
 class RandomPage extends StatelessWidget {
   const RandomPage({super.key});
@@ -11,7 +12,41 @@ class RandomPage extends StatelessWidget {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Image.asset('assets/appbar_logo.png'),
+          Stack(
+            children: [
+              Image.asset('assets/appbar_logo.png'),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return RandomWritePage();
+                      },
+                    ),
+                  );
+                },
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 85, left: 325),
+                      child: Icon(Icons.edit, size: 20),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 315),
+                      child: Text(
+                        '글쓰기',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
           Expanded(
             child: ListView(
               padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
