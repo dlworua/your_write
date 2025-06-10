a5a314e203278b0184d884cbb7826db8c71228a6
dlworua <leegaegyum@gmail.com>
2025-06-10 14:28:31 +0900

    feat: 사용되지 않는 임포트문 제거

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:your_write/ui/pages/home/home_page.dart';
 import 'package:your_write/ui/pages/main_page.dart';
 
 void main() {
