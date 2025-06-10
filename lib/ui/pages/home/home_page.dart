6c25b64eb075c9e56bca92a0762e75b146e3d324
dlworua <leegaegyum@gmail.com>
2025-06-10 14:37:00 +0900

    feat: 메인게시판에 글작성 아이콘 생성

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:your_write/ui/pages/home/widgets/home_post_list.dart';
+import 'package:your_write/ui/pages/write/main_write_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -11,7 +12,42 @@ class HomePage extends StatelessWidget {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Image.asset('assets/appbar_logo.png'),
+          Stack(
+            children: [
+              Image.asset('assets/appbar_logo.png'),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return MainWritePage();
+                      },
+                    ),
+                  );
+                },
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 85, left: 325),
+                      child: Icon(Icons.edit, size: 20),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 315),
+                      child: Text(
+                        '글쓰기',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+
           Expanded(
             child: ListView(
               padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
