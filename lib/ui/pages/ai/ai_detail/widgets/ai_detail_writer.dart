84dee624544b46fe75b3de55c8ce0775b3800425
dlworua <leegaegyum@gmail.com>
2025-06-13 14:22:40 +0900

    feat: 제목 생성자 만들어서 넣기

--- lib/ui/pages/ai/ai_detail/widgets/ai_detail_writer.dart
+++ lib/ui/pages/ai/ai_detail/widgets/ai_detail_writer.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 
 // 제목
 class AiDetailTitle extends StatelessWidget {
-  const AiDetailTitle({super.key});
+  final String title;
+
+  const AiDetailTitle({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Text(
-      '나의 여름',
+      title,
       style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
     );
   }
@@ -41,12 +43,13 @@ class AiDetailKeyword extends StatelessWidget {
 
 // 본문
 class AiDetailContent extends StatelessWidget {
-  const AiDetailContent({super.key});
+  final String content;
+  const AiDetailContent({super.key, required this.content});
 
   @override
   Widget build(BuildContext context) {
     return Text(
-      '나의 여름은 어디에 갔나\n 나의 여름은 떠나고\n 가을의 쓸쓸함이 날 기다리네\n나의 여름은 어디에 갔나\n 나의 여름은 떠나고\n 가을의 쓸쓸함이 날 기다리네\n나의 여름은 어디에 갔나\n 나의 여름은 떠나고\n 가을의 쓸쓸함이 날 기다리네\n나의 여름은 어디에 갔나\n 나의 여름은 떠나고\n 가을의 쓸쓸함이 날 기다리네\n나의 여름은 어디에 갔나\n 나의 여름은 떠나고\n 가을의 쓸쓸함이 날 기다리네\n나의 여름은 어디에 갔나\n 나의 여름은 떠나고\n 가을의 쓸쓸함이 날 기다리네\n나의 여름은 어디에 갔나\n 나의 여름은 떠나고\n 가을의 쓸쓸함이 날 기다리네\n나의 여름은 어디에 갔나\n 나의 여름은 떠나고\n 가을의 쓸쓸함이 날 기다리네\n나의 여름은 어디에 갔나\n 나의 여름은 떠나고\n 가을의 쓸쓸함이 날 기다리네\n나의 여름은 어디에 갔나\n 나의 여름은 떠나고\n 가을의 쓸쓸함이 날 기다리네\n ',
+      content,
       style: const TextStyle(fontSize: 18, height: 1.5),
       textAlign: TextAlign.center,
     );
