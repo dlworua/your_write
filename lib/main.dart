c8420877295d21c3233d680c1f99410bed19f166
dlworua <leegaegyum@gmail.com>
2025-06-14 22:04:48 +0900

    feat: main 함수에서 Firebase 초기화 코드 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:your_write/firebase_options.dart';
 import 'package:your_write/ui/pages/main_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   print(const String.fromEnvironment('GEMINI_API_KEY'));
   runApp(ProviderScope(child: MyApp()));
 }
