14f247062bac327ec5983717fb682428964754c2
dlworua <leegaegyum@gmail.com>
2025-06-16 17:29:44 +0900

    feat: homepost모델 생성

---
+++ lib/data/models/home_post.dart
@@ -0,0 +1,13 @@
+class HomePost {
+  final String title;
+  final String content;
+  final String keyword;
+  final String author;
+
+  HomePost({
+    required this.title,
+    required this.content,
+    required this.keyword,
+    required this.author,
+  });
+}
