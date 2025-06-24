2b15f146b703e75baa898f9fd6673a02413e02b0
dlworua <leegaegyum@gmail.com>
2025-06-24 12:23:42 +0900

    feat: 메인 다트 설정 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:your_write/firebase_options.dart';
+import 'package:your_write/app_routes.dart';
+import 'package:your_write/ui/pages/auth/email/auth_state_warpper.dart';
+import 'package:your_write/ui/pages/auth/email/email_login_page.dart';
 import 'package:your_write/ui/pages/main_page.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  // ignore: avoid_print
   print(const String.fromEnvironment('GEMINI_API_KEY'));
-  runApp(ProviderScope(child: MyApp()));
+  runApp(ProviderScope(child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -17,6 +19,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: MainPage());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AuthStateWrapper(
+        loggedInWidget: const MainPage(),
+        loggedOutWidget: const EmailLoginPage(),
+      ),
+      routes: AppRoutes.routes,
+      onGenerateRoute: AppRoutes.onGenerateRoute,
+    );
   }
 }
