02f0a23e8ba7541c95616b9b2805559340837b77
dlworua <leegaegyum@gmail.com>
2025-06-24 12:12:10 +0900

    feat: 주석 설명 추가하여 유저 레포지토리 완성

--- lib/data/repository/user_repository.dart
+++ lib/data/repository/user_repository.dart
@@ -2,22 +2,30 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import '../models/user_model.dart';
 
 class UserRepository {
-  // ignore: unused_field
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  // Firestore의 'users' 컬렉션 참조를 저장하는 변수
   final CollectionReference _usersCollection;
 
+  // 생성자: Firestore 인스턴스에서 'users' 컬렉션을 초기화
   UserRepository()
     : _usersCollection = FirebaseFirestore.instance.collection('users');
 
+  // 사용자 데이터를 Firestore에 저장하는 비동기 함수
   Future<void> saveUser(UserModel user) async {
+    // uid를 문서 ID로 사용해 사용자 데이터를 Map 형태로 저장
     await _usersCollection.doc(user.uid).set(user.toMap());
   }
 
+  // uid로 Firestore에서 사용자 데이터를 가져오는 비동기 함수
   Future<UserModel?> fetchUser(String uid) async {
+    // 해당 uid의 문서 가져오기
     final doc = await _usersCollection.doc(uid).get();
+
+    // 문서가 존재하면 UserModel 인스턴스로 변환 후 반환
     if (doc.exists) {
       return UserModel.fromMap(doc.data() as Map<String, dynamic>);
     }
+
+    // 문서가 없으면 null 반환
     return null;
   }
 }
